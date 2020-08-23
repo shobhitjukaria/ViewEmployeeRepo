eeef2bfb064469243d1a89804e4d29b8b69cd19e
Shobhit Jukaria <shobhitjukaria@gmail.com>
2020-08-23 23:11:30 +0530

    populating data in tabular form

---
+++ src/app/employees/employees.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-employees',
+  templateUrl: './employees.component.html',
+  styleUrls: ['./employees.component.css']
+})
+export class EmployeesComponent implements OnInit {
+
+  employees :any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    let employee= this.http.get("https://5f37efc3bbfd1e00160bf767.mockapi.io/ap/v1/users");
+    employee.subscribe((data)=>this.employees=data);
+  }
+
+}
